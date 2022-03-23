1df3eb76f31a3edeb539d57cf7a172193e9cfabb
Caio Soares <caiofasoares@gmail.com>
2022-03-23 12:18:34 -0300

    Initial Commit

---
+++ Swift iOS RPG Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift iOS RPG Project
+//
+//  Created by Caio Soares on 23/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift iOS RPG Project/Swift_iOS_RPG_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_iOS_RPG_ProjectApp.swift
+//  Swift iOS RPG Project
+//
+//  Created by Caio Soares on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_iOS_RPG_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
